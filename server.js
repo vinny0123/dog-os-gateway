98625e8615545ee06519579a475a078696e26a9d
vinny0123 <vinny.olito@icloud.com>
2026-04-22 10:26:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,9 @@ ${os.logic}
 
 // Railway port binding
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
+});
+
 });
